86935e6a81d734f146217bbeb4816729c4007de8
marcosvarela5 <marcosviesmugardos@gmail.com>
2021-09-23 13:01:19 +0200

    e1 test included

---
+++ DS-22-04-B1/test/e1/StringCountTest.java
@@ -0,0 +1,34 @@
+package e1;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringCountTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void countWords() {
+    }
+
+    @Test
+    void countChar() {
+    }
+
+    @Test
+    void countCharIgnoringCase() {
+    }
+
+    @Test
+    void isPasswordSafe() {
+    }
+}
