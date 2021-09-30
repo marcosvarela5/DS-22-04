cc433c272e8ee07432dd3ab82e9788989d966743
marcosvarela5 <marcosviesmugardos@gmail.com>
2021-09-30 19:07:32 +0200

    Doing down the slope

--- DS-22-04-B1/src/e2/Slopes.java
+++ DS-22-04-B1/src/e2/Slopes.java
@@ -4,14 +4,28 @@ public class Slopes {
 
     public static int downTheSlope(char[][] slopeMap, int right, int down) {
         boolean go = true;
-        int row = 0, column = 0;
+        int rows = 0, columns = 0;
+        int cont = 0;
 
         if(!MatrixUtils.isMapSquare(slopeMap)) throw new IllegalArgumentException("Map is not square");
         if(!MatrixUtils.areMapCharactersValid(slopeMap)) throw new IllegalArgumentException("Map characters are not valid");
         if(!MatrixUtils.isRightValid(slopeMap, right)) throw new IllegalArgumentException("Right is not valid");
         if(!MatrixUtils.isDownValid(slopeMap, down)) throw new IllegalArgumentException("Down is not valid");
 
-        return 0;
+        while(go){
+
+
+            slopeMap[rows][columns] = slopeMap[rows][columns + right];
+            for(columns = 0; columns < slopeMap[rows].length; columns++)
+                if(slopeMap[0][columns] == '#') cont++;
+
+
+            slopeMap[rows][columns] = slopeMap[rows + down][columns]; // aquí falla
+            for(rows = 0; rows < slopeMap.length; rows++)
+                if(slopeMap[rows][columns] == '#') cont++;
+
+        }
+        return cont;
     }
 
     public static int jumpTheSlope(char[][] slopeMap, int right, int down){
